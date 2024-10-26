71f8fcd99904a2e308c4ef7ae48d10f200d33d58
Cedric Joshua Palapuz <115864258+Corelocked@users.noreply.github.com>
2024-10-26 17:33:23 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import joblib
+import numpy as np
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
+
+# Load the saved model
+model = joblib.load('iris_model.joblib')
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['features']
+    prediction = model.predict([data])[0]
+    print("Prediction received:", data)  # For debugging
+    return jsonify({'prediction': int(prediction)})
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
